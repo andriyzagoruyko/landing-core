51285a4509416b422d1f4551d4567b9bd752c6af
Andriy <arwel239@gmail.com>
2020-11-22 15:22:15 +0200

    Install Laravel with React

---
+++ resources/js/app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Example from '../components/Example'
+
+class App extends React.Component{
+    render(){
+        return <Example/>
+    }
+}
+
+export default App;
